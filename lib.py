9de146c39595413d62a1adce4fe8d37ec3d52719
Phil Gonzalez <pgonzalez115@gmail.com>
2020-03-14 23:40:15 -0600

    Update report

--- lib.py
+++ lib.py
@@ -9,3 +9,19 @@ def get_or_update_source_data():
     else:
         print("Updating source data")
         os.system("cd COVID-19 && git pull origin master")
+
+
+def logistic4(x, A, B, C, D):
+    """4PL lgoistic equation."""
+    return ((A-D)/(1.0+((x/C)**B))) + D
+
+def residuals(p, y, x):
+    """Deviations of data from fitted 4PL curve"""
+    A,B,C,D = p
+    err = y-logistic4(x, A, B, C, D)
+    return err
+
+def peval(x, p):
+    """Evaluated value at x with current parameters."""
+    A,B,C,D = p
+    return logistic4(x, A, B, C, D)
